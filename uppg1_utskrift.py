411b9525ca1fb61bc2545b126787e0a5c923e213
gabbimon69-ai <gabbimon69@gmail.com>
2025-08-26 11:00:57 +0200

    klar

--- uppg1_utskrift.py
+++ uppg1_utskrift.py
@@ -5,5 +5,9 @@ Programmet ska sedan skriva ut en mening som använder dessa variabler
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 (Hampus) gillar att (kolla på film) och (spela kortspel)
 """
+username= input("username")
+Thing1= input("thing i like")
+Thing2= input("other thing i like")
 
+print (f"hello, I'm {username} and i like {Thing1} the most and {Thing2} is number two on my list")
 
