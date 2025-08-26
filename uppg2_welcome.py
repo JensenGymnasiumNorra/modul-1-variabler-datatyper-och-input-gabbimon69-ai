695edf03666d72e8609c5826a8f8a795164494ad
gabbimon69-ai <gabbimon69@gmail.com>
2025-08-26 11:10:27 +0200

    to easy

--- uppg2_welcome.py
+++ uppg2_welcome.py
@@ -5,3 +5,6 @@ Välkomna sedan användaren till programmet.
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Hej (Hampus), välkommen till programmet!
 """
+
+username=input ("username. ")
+print(f"Hello {username}, Wellcome to the program")
