8c4c0ce464ac71d76c0818bc824a50e7c168b4e1
gabbimon69-ai <gabbimon69@gmail.com>
2025-08-28 09:59:43 +0200

    klar

--- uppg5_more_variables.py
+++ uppg5_more_variables.py
@@ -12,3 +12,11 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Välkommen till mitt program (Oskar). Du och (Hampus) är (53) år tillsammans.
 
 """
+
+name1=("Gabriel")
+year1=("17")
+name2= input("What is your name. ")
+year2=int(input("How old are you. "))
+age= year2-17
+age1=year2+17
+print(f"Hello {name2}. You are {year2} years old and are {age} years older then {name1} who is {year1} years old. and together you are {age1} years old")
